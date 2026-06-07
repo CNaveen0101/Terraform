2124ff379810f5fe9acfc7e1b1e1cd45a258b9bd
CNaveen0101 <143951163+CNaveen0101@users.noreply.github.com>
2026-06-07 12:07:46 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "ami" {
+description = "value of ami"
+}
+
+variable "instance_type"{
+description = "value of instance type"
+}
+
+variable "key_name" {
+description = "Value of Key Name"
+}
+
+variable "subnet_id" {
+description = "Value of Subnet id"
+}
+
+variable "vpc_security_group_ids" {
+description = "Value of Security group"
+}
